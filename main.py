174afd3aa602507ab686a4c28375ca7fd09a3984
pleasepleasepleases2 <docinhozmalu@gmail.com>
2024-10-13 20:37:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -878,7 +878,85 @@ def perfil_config(call):
         bot.edit_message_text("Escolha o que deseja ativar/desativar no perfil:", chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
     finally:
         fechar_conexao(cursor, conn)
+@bot.message_handler(commands=['doar'])
+def doar(message):
+    try:
+        print("Comando doar acionado")
+        chat_id = message.chat.id
+        eu = message.from_user.id
+        args = message.text.split()
 
+        if len(args) < 2:
+            bot.send_message(chat_id, "Formato incorreto. Use /doar <quantidade> <ID_da_carta> ou /doar all <ID_da_carta>")
+            return
+
+        doar_todas = False
+        doar_uma = False
+
+        if args[1].lower() == 'all':
+            doar_todas = True
+            minhacarta = int(args[2])
+        elif len(args) == 2:
+            doar_uma = True
+            minhacarta = int(args[1])
+        else:
+            quantidade = int(args[1])
+            minhacarta = int(args[2])
+
+        conn, cursor = conectar_banco_dados()
+        qnt_carta = verifica_inventario_troca(eu, minhacarta)
+        if qnt_carta > 0:
+            if doar_todas:
+                quantidade = qnt_carta
+            elif doar_uma:
+                quantidade = 1
+            elif quantidade > qnt_carta:
+                bot.send_message(chat_id, f"Você não possui {quantidade} unidades dessa carta.")
+                return
+
+            destinatario_id = None
+            nome_destinatario = None
+
+            if message.reply_to_message and message.reply_to_message.from_user:
+                destinatario_id = message.reply_to_message.from_user.id
+                nome_destinatario = message.reply_to_message.from_user.first_name
+
+            # Verificar se o destinatário é o bot
+            if destinatario_id == int(API_TOKEN.split(':')[0]):
+                bot.send_message(chat_id, "Pr-Pra mim? 😳 Muito obrigada, mas não acho que seja de bom tom um bot aceitar doação tão generosa... 😢 Talvez você deva procurar um camponês de verdade...")
+                return
+
+            if not destinatario_id:
+                bot.send_message(chat_id, "Você precisa responder a uma mensagem para doar a carta.")
+                return
+
+            doacao_suspeita, total_trocas, total_anterior, total_atual = verificar_doacao_suspeita(cursor, eu, destinatario_id, minhacarta, quantidade)
+            if doacao_suspeita:
+                alerta = (f"⚠️ Doação suspeita! ⚠️\n"
+                          f"Doação da carta {minhacarta} entre {message.from_user.first_name} e {nome_destinatario}.\n"
+                          f"Quantidade doada anteriormente: {total_anterior}\n"
+                          f"Quantidade doada agora: {total_atual}\n"
+                          f"Número de trocas entre os usuários: {total_trocas}")
+                enviar_alerta_para_grupo(alerta)
+
+            nome_carta = obter_nome(minhacarta)
+            qnt_str = f"uma unidade da carta" if quantidade == 1 else f"{quantidade} unidades da carta"
+            texto = f"Olá, {message.from_user.first_name}!\n\nVocê tem {qnt_carta} unidades da carta: {minhacarta} — {nome_carta}.\n\n"
+            texto += f"Deseja doar {qnt_str} para {nome_destinatario}?"
+
+            keyboard = telebot.types.InlineKeyboardMarkup()
+            keyboard.row(
+                telebot.types.InlineKeyboardButton(text="Sim", callback_data=f'cdoacao_{eu}_{minhacarta}_{destinatario_id}_{quantidade}'),
+                telebot.types.InlineKeyboardButton(text="Não", callback_data=f'ccancelar_{eu}')
+            )
+
+            bot.send_message(chat_id, texto, reply_markup=keyboard)
+        else:
+            bot.send_message(chat_id, "Você não pode doar uma carta que não possui.")
+
+    except Exception as e:
+        newrelic.agent.record_exception()    
+        print(f"Erro durante o comando de doação: {e}")
 def atualizar_petalas(id_usuario):
     """Atualiza o número de pétalas do usuário de acordo com o tempo decorrido e se é VIP."""
     print(f"DEBUG: Iniciando atualização de pétalas para o usuário {id_usuario}.")
@@ -1390,85 +1468,6 @@ def callback_subcategoria_handler(call):
     finally:
         fechar_conexao(cursor, conn)
 
-@bot.message_handler(commands=['doar'])
-def doar(message):
-    try:
-        print("Comando doar acionado")
-        chat_id = message.chat.id
-        eu = message.from_user.id
-        args = message.text.split()
-
-        if len(args) < 2:
-            bot.send_message(chat_id, "Formato incorreto. Use /doar <quantidade> <ID_da_carta> ou /doar all <ID_da_carta>")
-            return
-
-        doar_todas = False
-        doar_uma = False
-
-        if args[1].lower() == 'all':
-            doar_todas = True
-            minhacarta = int(args[2])
-        elif len(args) == 2:
-            doar_uma = True
-            minhacarta = int(args[1])
-        else:
-            quantidade = int(args[1])
-            minhacarta = int(args[2])
-
-        conn, cursor = conectar_banco_dados()
-        qnt_carta = verifica_inventario_troca(eu, minhacarta)
-        if qnt_carta > 0:
-            if doar_todas:
-                quantidade = qnt_carta
-            elif doar_uma:
-                quantidade = 1
-            elif quantidade > qnt_carta:
-                bot.send_message(chat_id, f"Você não possui {quantidade} unidades dessa carta.")
-                return
-
-            destinatario_id = None
-            nome_destinatario = None
-
-            if message.reply_to_message and message.reply_to_message.from_user:
-                destinatario_id = message.reply_to_message.from_user.id
-                nome_destinatario = message.reply_to_message.from_user.first_name
-
-            # Verificar se o destinatário é o bot
-            if destinatario_id == int(API_TOKEN.split(':')[0]):
-                bot.send_message(chat_id, "Pr-Pra mim? 😳 Muito obrigada, mas não acho que seja de bom tom um bot aceitar doação tão generosa... 😢 Talvez você deva procurar um camponês de verdade...")
-                return
-
-            if not destinatario_id:
-                bot.send_message(chat_id, "Você precisa responder a uma mensagem para doar a carta.")
-                return
-
-            doacao_suspeita, total_trocas, total_anterior, total_atual = verificar_doacao_suspeita(cursor, eu, destinatario_id, minhacarta, quantidade)
-            if doacao_suspeita:
-                alerta = (f"⚠️ Doação suspeita! ⚠️\n"
-                          f"Doação da carta {minhacarta} entre {message.from_user.first_name} e {nome_destinatario}.\n"
-                          f"Quantidade doada anteriormente: {total_anterior}\n"
-                          f"Quantidade doada agora: {total_atual}\n"
-                          f"Número de trocas entre os usuários: {total_trocas}")
-                enviar_alerta_para_grupo(alerta)
-
-            nome_carta = obter_nome(minhacarta)
-            qnt_str = f"uma unidade da carta" if quantidade == 1 else f"{quantidade} unidades da carta"
-            texto = f"Olá, {message.from_user.first_name}!\n\nVocê tem {qnt_carta} unidades da carta: {minhacarta} — {nome_carta}.\n\n"
-            texto += f"Deseja doar {qnt_str} para {nome_destinatario}?"
-
-            keyboard = telebot.types.InlineKeyboardMarkup()
-            keyboard.row(
-                telebot.types.InlineKeyboardButton(text="Sim", callback_data=f'cdoacao_{eu}_{minhacarta}_{destinatario_id}_{quantidade}'),
-                telebot.types.InlineKeyboardButton(text="Não", callback_data=f'ccancelar_{eu}')
-            )
-
-            bot.send_message(chat_id, texto, reply_markup=keyboard)
-        else:
-            bot.send_message(chat_id, "Você não pode doar uma carta que não possui.")
-
-    except Exception as e:
-        newrelic.agent.record_exception()    
-        print(f"Erro durante o comando de doação: {e}")
 
 @bot.message_handler(commands=['ervadaninha'])
 def listar_bloqueios(message):

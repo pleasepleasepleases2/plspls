0066ecac4181c9677614823dea5b1b3f9471fa38
pleasepleasepleases2 <docinhozmalu@gmail.com>
2024-10-24 22:43:58 -0300

    Update gnome.py

--- gnome.py
+++ gnome.py
@@ -45,20 +45,7 @@ def clear_user_state(user_id, command):
     finally:
         fechar_conexao(cursor, conn)
 
-def create_navigation_markup(current_index, total_count):
-    markup = types.InlineKeyboardMarkup()
-    buttons = []
 
-    if current_index > 0:
-        prev_button = types.InlineKeyboardButton(text="⬅", callback_data=f"change_page_{current_index-1}")
-        buttons.append(prev_button)
-    
-    if current_index < total_count - 1:
-        next_button = types.InlineKeyboardButton(text="➡", callback_data=f"change_page_{current_index+1}")
-        buttons.append(next_button)
-    
-    markup.add(*buttons)
-    return markup
 
 def send_message_with_buttons(chat_id, idmens, mensagens, current_index=0, reply_to_message_id=None):
     total_count = len(mensagens)
@@ -78,28 +65,4 @@ def send_message_with_buttons(chat_id, idmens, mensagens, current_index=0, reply
     else:
         bot.send_message(chat_id, "Não há mais personagens disponíveis.")
         clear_user_state(chat_id, 'gnomes')
-        
-
-def save_state(user_id, command, data, chat_id, message_id):
-    conn, cursor = conectar_banco_dados()
-
-    try:
-        cursor.execute("REPLACE INTO user_state (user_id, command, data, chat_id, message_id) VALUES (%s, %s, %s, %s, %s)",
-                       (user_id, command, json.dumps(data), chat_id, message_id))
-        conn.commit()
-    finally:
-        fechar_conexao(cursor, conn)
-
-def create_navegacao_markup(pagina_atual, total_paginas):
-    markup = types.InlineKeyboardMarkup()
-    if pagina_atual > 1:
-        prev_button_text = f"< Anterior"
-        prev_button_callback = f"prev_button_{pagina_atual - 1}_{total_paginas}"
-        markup.add(types.InlineKeyboardButton(text=prev_button_text, callback_data=prev_button_callback))
-
-    if pagina_atual < total_paginas:
-        next_button_text = f"Próximo >"
-        next_button_callback = f"next_button_{pagina_atual + 1}_{total_paginas}"
-        markup.add(types.InlineKeyboardButton(text=next_button_text, callback_data=next_button_callback))
-    return markup
-        
+      

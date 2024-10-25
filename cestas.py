efcf5b6d8c7b46a39de3ad7325f59664c05beffb
pleasepleasepleases2 <docinhozmalu@gmail.com>
2024-10-25 04:19:33 -0300

    Update cestas.py

--- cestas.py
+++ cestas.py
@@ -455,25 +455,6 @@ def mostrar_pagina_cesta_cf(message, categoria, id_usuario, pagina_atual, total_
     finally:
         fechar_conexao(cursor, conn)
 
-# Função para criar a navegação com botões de "⏪️", "⬅️", "➡️" e "⏩️"
-def criar_markup_cesta(pagina_atual, total_paginas, subcategoria, tipo, id_usuario_original):
-    markup = telebot.types.InlineKeyboardMarkup(row_width=4)
-
-    # Navegação circular
-    pagina_anterior = total_paginas if pagina_atual == 1 else pagina_atual - 1
-    pagina_proxima = 1 if pagina_atual == total_paginas else pagina_atual + 1
-
-    # Botões de navegação
-    markup.row(
-        telebot.types.InlineKeyboardButton(text="⏪️", callback_data=f"cesta_{tipo}_1_{subcategoria}_{id_usuario_original}"),
-        telebot.types.InlineKeyboardButton(text="⬅️", callback_data=f"cesta_{tipo}_{pagina_anterior}_{subcategoria}_{id_usuario_original}"),
-        telebot.types.InlineKeyboardButton(text="➡️", callback_data=f"cesta_{tipo}_{pagina_proxima}_{subcategoria}_{id_usuario_original}"),
-        telebot.types.InlineKeyboardButton(text="⏩️", callback_data=f"cesta_{tipo}_{total_paginas}_{subcategoria}_{id_usuario_original}")
-    )
-
-    return markup
-
-
 def adicionar_quantidade_cartas(quantidade_carta):
     if isinstance(quantidade_carta, str):
         quantidade_carta = int(quantidade_carta)

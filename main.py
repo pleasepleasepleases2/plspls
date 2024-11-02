dc384fa466b87c59129a2bca7c4e7588da24982e
pleasepleasepleases2 <docinhozmalu@gmail.com>
2024-11-01 23:33:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -457,14 +457,14 @@ def callback_evento_navegacao(call):
         if tipo == 's':
             # Personagens possuídos no evento
             ids_personagens = obter_ids_personagens_evento_inventario(id_usuario, evento, subcategoria=None)
-            total_personagens = obter_total_personagens_evento_subcategoria(evento, subcategoria=None)
+            total_personagens = obter_total_personagens_evento(evento, subcategoria=None)
             total_paginas = (len(ids_personagens) // 15) + (1 if len(ids_personagens) % 15 > 0 else 0)
             mostrar_pagina_evento_s(call.message, evento, id_usuario, pagina, total_paginas, ids_personagens, total_personagens, nome_usuario, call)
 
         elif tipo == 'f':
             # Personagens faltantes no evento
             ids_personagens_faltantes = obter_ids_personagens_evento_faltantes(id_usuario, evento, subcategoria=None)
-            total_personagens = obter_total_personagens_evento_subcategoria(evento, subcategoria=None)
+            total_personagens = obter_total_personagens_evento(evento, subcategoria=None)
             total_paginas = (len(ids_personagens_faltantes) // 15) + (1 if len(ids_personagens_faltantes) % 15 > 0 else 0)
             mostrar_pagina_evento_f(call.message, evento, id_usuario, pagina, total_paginas, ids_personagens_faltantes, total_personagens, nome_usuario, call)
 
@@ -3701,13 +3701,13 @@ def processar_evento(message):
         # Definir parâmetros da função de exibição com base no tipo
         if tipo == 's':
             ids_personagens = obter_ids_personagens_evento_inventario(id_usuario, evento, subcategoria=None)
-            total_personagens = obter_total_personagens_evento_subcategoria(evento, subcategoria=None)
+            total_personagens = obter_total_personagens_evento(evento, subcategoria=None)
             total_paginas = (len(ids_personagens) // 15) + (1 if len(ids_personagens) % 15 > 0 else 0)
             mostrar_pagina_evento_s(message, evento, id_usuario, 1, total_paginas, ids_personagens, total_personagens, nome_usuario)
         
         elif tipo == 'f':
             ids_personagens_faltantes = obter_ids_personagens_evento_faltantes(id_usuario, evento, subcategoria=None)
-            total_personagens = obter_total_personagens_evento_subcategoria(evento, subcategoria=None)
+            total_personagens = obter_total_personagens_evento(evento, subcategoria=None)
             total_paginas = (len(ids_personagens_faltantes) // 15) + (1 if len(ids_personagens_faltantes) % 15 > 0 else 0)
             mostrar_pagina_evento_f(message, evento, id_usuario, 1, total_paginas, ids_personagens_faltantes, total_personagens, nome_usuario)
 
